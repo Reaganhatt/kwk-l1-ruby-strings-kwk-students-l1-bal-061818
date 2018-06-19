39cf4bb01ee8453ce34afe969f9763cfe5d0aa5d
Reaganhatt <reagan.hatt@gmail.com>
2018-06-19 13:11:06 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,9 +1,11 @@
 name = "guest_name"
-name.gets.chomp
 party = "party_name"
-party.gets.chomp
 date = "date"
 time = "time"
 host = "host_name"
-host.gets.chomp
 puts "Dear #{name}, You are cordially invited to #{party} on #{date} at #{time}. Sincerely, #{host}."
+puts "Your name" (Reagan)
+puts "Your party" (The Coolest Party)
+puts "Your date" (February 14)
+puts "Your time" (8pm)
+puts "Your host" (Tim)
