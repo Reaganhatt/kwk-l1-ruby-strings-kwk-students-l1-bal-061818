bfecc51007c560b752c7a4c6a985ab318b6c4b70
Reaganhatt <reagan.hatt@gmail.com>
2018-06-19 13:12:50 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -4,7 +4,7 @@ date = "date"
 time = "time"
 host = "host_name"
 puts "Dear #{name}, You are cordially invited to #{party} on #{date} at #{time}. Sincerely, #{host}."
-puts "Your name" Reagan)
+puts "Your name" Reagan
 puts "Your party" (The Coolest Party)
 puts "Your date" (February 14)
 puts "Your time" (8pm)
